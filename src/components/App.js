0f1f8ebc1670ad1e74ee983e6ac72d1ba3f8311b
Vadzim <vadzimk@hotmail.com>
2019-02-13 17:53:43 -0800

    make searchBar pass data to the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = function(){
-	return (
-		<div className="ui container" style={{marginTop: "10px"}}>
-			<SearchBar />
-		</div>
-		);
+class App extends React.Component {
+
+	onSearchSubmit(term){
+		console.log(term);		
+	}
+
+	render(){
+		return (
+			<div className="ui container" style={{marginTop: "10px"}}>
+				<SearchBar whenSubmitted={this.onSearchSubmit} />
+			</div>
+			);
+		}
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React from 'react';
 
-class SearchBar extends React.Component{
+class SearchBar extends React.Component {
+  state = { term: '' };
 
-	render(){
-		return (
-			<div className="ui segment">
-				<form className="ui form">
-					<div className="field">
-						<label>Image Search</label>
-						<input type="text" />
-					</div>
-				</form>
-			</div>
-			);
-	}
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.whenSubmitted(this.state.term);
+//in the App component (parent) we pass props to the SearchBar component (child). onFormSubmit method is called when user hits submit button (event handler inside the form tag). onFormSubmit takes the props object and calls its whensubmitted method, that was declared in the App component. In such a way we pass data (the search term) from the child to the parent.
+
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+              placeholder="type your search term here"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
